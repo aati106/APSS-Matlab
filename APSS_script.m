a3099ce8fd6eca11eea6f6fffcd1ecc0aaa20b91
Anthony <aati106@aucklanduni.ac.nz>
2023-09-09 00:44:25 +1200

    accounting for internal_heat_dissipation

--- APSS_script.m
+++ APSS_script.m
@@ -9,6 +9,7 @@ area_surface = 0.01;
 altitude = 500;
 earth_radius = 6378;
 angle = 0;
+internal_heat_dissipation=6.9684;
 
 incident_angle = 0:1:180;
 
@@ -23,7 +24,7 @@ heat_ir_flux = emissivity * area_surface * view_factor * solar_constant;
 heat_albedo= absorptivity * area_surface * albedo_factor * view_factor * solar_constant;
 solar_heat = absorptivity * area_surface * solar_constant * cosd(angle);
 
-h_out = heat_albedo + solar_heat + heat_ir_flux + heat_IR_temperature;
+h_out = heat_albedo + solar_heat + heat_ir_flux + heat_IR_temperature + internal_heat_dissipation;
 temperature(count) = (h_out/(area_surface * emissivity * stefan_boltzmann_constant))^(0.25);
 
    count=count+1;
@@ -52,7 +53,7 @@ heat_ir_flux = emissivity * area_surface * view_factor * solar_constant;
 heat_albedo= absorptivity * area_surface * albedo_factor * view_factor * solar_constant;
 solar_heat = absorptivity * area_surface * solar_constant * cosd(angle);
 
-h_out = heat_albedo + solar_heat + heat_ir_flux + heat_IR_temperature;
+h_out = heat_albedo + solar_heat + heat_ir_flux + heat_IR_temperature + internal_heat_dissipation;
 temperature(count) = (h_out/(area_surface * emissivity * stefan_boltzmann_constant))^(0.25);
 
    count=count+1;
