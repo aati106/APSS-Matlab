f0b995ff1f1066d7c5d4d6b67aa205a253bbccdb
Anthony <aati106@aucklanduni.ac.nz>
2023-09-09 00:40:46 +1200

    Making figure for altitude changes

--- APSS_script.m
+++ APSS_script.m
@@ -30,10 +30,39 @@ temperature(count) = (h_out/(area_surface * emissivity * stefan_boltzmann_consta
 end 
 
 
-
-% Code chunk for plotting points
+figure(1);
 plot (incident_angle,temperature);
 title("Steady State Temperature at different incident Angles")
 xlabel("Incident Angle (degrees)");
 ylabel("Temperature (kelvin)");
+
+
+angle = 0;
+count = 1;
+altitude = 300;
+altitude_range=300:1:700;
+
+for i = 300:700
+  view_factor = (altitude + earth_radius)/earth_radius;
+  altitude = altitude + 1;
+
+
+heat_IR_temperature = stefan_boltzmann_constant * emissivity * view_factor * area_surface * 255^4;
+heat_ir_flux = emissivity * area_surface * view_factor * solar_constant;
+heat_albedo= absorptivity * area_surface * albedo_factor * view_factor * solar_constant;
+solar_heat = absorptivity * area_surface * solar_constant * cosd(angle);
+
+h_out = heat_albedo + solar_heat + heat_ir_flux + heat_IR_temperature;
+temperature(count) = (h_out/(area_surface * emissivity * stefan_boltzmann_constant))^(0.25);
+
+   count=count+1;
+end 
+
+figure(2);
+plot (altitude_range,temperature);
+title("Steady State Temperature at different altitudes")
+xlabel("Altitude (kilometres)");
+ylabel("Temperature (kelvin)");
+
+
 
